2896805914cefb989699f4bc462ecfdf066180e0
itisjustme (Guzairova Olga) <guzairovaolga21@gmail.com>
2026-05-29 11:23:48 +0300

    done main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,5 +1,28 @@
-import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 export default function HomePage() {
+    const { user } = useAuth()
 
+    return (
+        <section>
+            <h1>
+                Simple Archive
+            </h1>
+
+            {!user ? (
+                <h3>
+                    Ищите документы на интересующие темы
+                </h3>
+            ) : (
+                <h3>
+                    Начните {" "}
+                    <Link to="/racks">
+                        поиск
+                    </Link>
+                    {" "} прямо сейчас
+                </h3>
+            )}
+        </section>
+    )
 }
